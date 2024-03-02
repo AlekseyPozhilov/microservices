e6e79c19444304f5e59533a8a77f69743ae904f7
Aleksey Pozhilov <lpozhilov17@mail.ru>
2024-03-02 20:14:04 +0300

    Add entity for resource and song

---
+++ songService/src/main/java/com/belhard/msvc_pozhilov/entity/SongEntity.java
@@ -0,0 +1,32 @@
+package com.belhard.msvc_pozhilov.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class SongEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String artist;
+    private String album;
+    private Integer length;
+    private Integer resourceID;
+    private Integer year;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdTimestamp;
+}
